e20cac581f481a8df754a73cf6b1316a01ab752d
王心 <wangxin@primeton.com>
2021-08-22 14:19:21 +0800

    <feature>[]初始化java包

---
+++ src/Math.java
@@ -0,0 +1,11 @@
+/**
+ * @Author wangxin
+ * @Description
+ * @Date create in 14:15 2021/8/22
+ * @Modified
+ */
+public class Math {
+    public static void main(String[] args){
+        System.out.println("hello JVM");
+    }
+}
